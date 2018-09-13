96eadf3950a6337ebdbc810458529ec37e00d417
dflegg <43074341+dflegg@users.noreply.github.com>
2018-09-13 00:00:50 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function creates a special "matrix" that then solves for its inverse and caches the answer
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  ## allows user to set matrix input for the function i.e var$set(matrix(...))
+  set <- function(mat){
+    x <<- mat  
+    invmat <<- NULL
+  }
+  get <- function() x
+  set_invmat <- function(invmat)
+  ## solves inverse of matrix using solve()  
+    invmat <<- solve(x)                      
+  ## Once called get_invmat provides the inverse if calculated
+  get_invmat <- function() 
+    invmat
+  
+  list(set = set, get = get, set_invmat = set_invmat, get_invmat = get_invmat) 
+  ## Allows for '$' operator to be used when executing commands in the console
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse special "matrix" from the function makeCacheMatrix
+## If the inverse of the matrix has already been solved cacheSolve will retrieve the matrix from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Variable is input that was assigned value of makeCacheMatrix() in console
+  invmat <- x$get_invmat()
+    if(!is.null(invmat)){
+      print("Getting cached data...")     ##Retrieves data from cache
+      invmat
+    }
+    data <- x$get()
+    invmat <- solve(data,...)
+    x$set_invmat(invmat)
+    invmat
+## Returns inverse matrix of 'x'
 }
